78f2e5468ca1c456be2a7238e26a7a7e7b0fff42
hdntc <realcirno@protonmail.com>
2022-11-06 21:20:42 +0000

    add navbar

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react";
+
+const Navbar = (props) => {
+    return <Flex
+    height="40px"
+    bgColor="#ED3B3B"
+    width="100vw"
+    >
+        
+    </Flex>
+}
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import NextLink from "next/link";
 import { Link } from "@chakra-ui/react";
 
+import Navbar from "../components/Navbar";
+
 export default function Home() {
-    return <div>
+    return <div
+    height="100vh"
+    width="100vw"
+    >
+        <Navbar/>
         Good day
         <NextLink href="/about" legacyBehavior passHref>
             <Link>
