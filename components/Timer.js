0b64bfc018bff9e0e348a0b4561b0dd0a99c20da
hdntc <realcirno@protonmail.com>
2022-11-21 12:21:53 +0000

    aiosdjsioadj

---
+++ components/Timer.js
@@ -0,0 +1,156 @@
+import { Flex, Box, Button, Slider, SliderFilledTrack, SliderTrack, SliderThumb, Text } from "@chakra-ui/react";
+import { MdPause, MdPlayArrow } from "react-icons/md";
+import { useState, useEffect } from "react";
+
+const Timer = ({youtubeRef}) => {
+    const [time, setTime] = useState(1200);
+
+    const timeToMMSS = (seconds) => {
+        const s = seconds % 60;
+        const m = (seconds - s) / 60;
+
+        return m.toString().padStart(2, "0") + ":" + s.toString().padStart(2, "0")
+    }
+
+    return <Box
+    height="250px"
+    width="500px"
+    borderRadius="32px"
+    bgColor="#ED3B3B"
+    flexDir="column"
+    >
+        <Flex
+        width="100%"
+        height="200px"
+        fontSize="55px"
+        fontWeight="bold"
+        color="white"
+        justify="center"
+        align="center"
+        >
+            <Button
+            width="32px"
+            height="32px"
+            size="sm"
+            color="black"
+            >
+                a
+            </Button>
+            {timeToMMSS(time)}
+            <Button
+            width="40px"
+            height="40px"
+            >
+                a
+            </Button>
+        </Flex>
+        <Flex
+        width="100%"
+        height="50px"
+        borderBottomRightRadius="32px"
+        borderBottomLeftRadius="32px"
+        bgColor="white"
+        border="1px solid #ED3B3B"
+        justify="space-between"
+        >
+            <Flex
+            height="100%"
+            width="90px"
+            borderRight="1px solid #ED3B3B"
+            justify="center"
+            align="center"
+            fontSize="28px"
+            >
+                <MdPlayArrow/>
+            </Flex>
+            <Flex
+            height="50px"
+            width="100%"
+            flexDir="column"
+            >
+                <Flex
+                height="30px"
+                width="100%"
+                m="5px"
+                mb="3px"
+                >
+                    <Box
+                    height="30px"
+                    width="30px"
+                    borderRadius="full"
+                    bgColor="black"
+                    mr="5px"
+                    >
+                    </Box>
+
+                    <Flex
+                    flexDir="column"
+                    height="30px"
+                    width="100%"
+                    top="-3px"
+                    position="relative"
+                    >
+                        <Text
+                        height="18px"
+                        fontSize="17px"
+                        top="50px"
+                        >
+                            Rick Astley - Never Gonna Give You Up 
+                        </Text>
+                        <Text
+                        height="12px"
+                        fontSize="11px"
+                        >
+                            pomodori_lover34
+                        </Text>
+                    </Flex>
+                </Flex>
+                <Slider
+                min={0}
+                max={100}
+                height="10px"
+                borderRadius="0"
+                >
+                    <SliderTrack
+                    height="10px"
+                    borderRadius="0"
+                    >
+                        <SliderFilledTrack
+                        bgColor="#ED3B3B"
+                        />
+                    </SliderTrack>
+                </Slider>
+            </Flex>
+            <Flex
+            height="100%"
+            width="90px"
+            borderLeft="1px solid #ED3B3B"
+            justify="center"
+            align="center"
+            >
+                <Slider
+                min={0}
+                max={100}
+                width="60%"
+                borderRadius="0"
+                >
+                    <SliderTrack
+                    height="5px"
+                    borderRadius="0"
+                    >
+                        <SliderFilledTrack
+                        bgColor="#ED3B3B"
+                        />
+                    </SliderTrack>
+                    <SliderThumb
+                    bgColor="red"
+                    width="8px"
+                    height="8px"
+                    />
+                </Slider>
+            </Flex>
+        </Flex>
+    </Box>
+}
+
+export default Timer;
--- pages/sessions/[sid].js
+++ pages/sessions/[sid].js
@@ -1,13 +1,46 @@
 import { useRouter } from "next/router"
-import { Flex } from "@chakra-ui/react";
+import { useRef } from "react";
+import { Flex, Box, Button } from "@chakra-ui/react";
+import YouTube from "react-youtube";
+
+import Timer from "../../components/Timer.js";
+import Navbar from "../../components/Navbar.js";
 
 const SessionPage = (props) => {
     const router = useRouter();
+    const youtube = useRef();
     const { sid } = router.query;
 
-    return <Flex>
-        {sid}
-    </Flex>
+    return <Box
+    height="100vh"
+    width="100vw"
+    >
+        <Navbar/>
+        <Timer youtubeRef={youtube}/>
+
+        <Box
+        display="none"
+        >
+            <YouTube
+            videoId="vMAoP7Jlnw4"
+            iframeClassName="youtubePlayer"
+            ref={youtube}
+            opts={{
+                width: 800,
+                height: 450,
+                playerVars: {
+                    autoplay: 1
+                }
+            }}
+            onStateChange={(e) => alert(e.data)}
+            />
+        </Box>
+        <Button onClick={() => {
+            youtube.current.internalPlayer.loadVideoById("ABeNe3E6c54") // PAcCIkATg7c
+        }}>
+            aodijasod
+        </Button>
+    </Box>
 }
 
 export default SessionPage;
